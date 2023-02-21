ca1ee3cac6e2898d90acae37f327c8412f7a7cb8
simonjung <sjung@dendisoftware.com>
2023-02-20 21:24:49 -0500

    Added LIFX example code

--- run.py
+++ run.py
@@ -1,2 +1,9 @@
-# MY CODE
-print('hello world')
+import requests
+
+token = "c9f4bd850a7c8502dd5867967134bc916349025fd1a4a9ae5ced534df5376bb6"
+
+headers = {
+    "Authorization": "Bearer %s" % token,
+}
+
+response = requests.post('https://api.lifx.com/v1/lights/all/toggle', headers=headers)
