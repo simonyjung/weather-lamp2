639253f82afcb7644ba4df3a156f067dbf8a58ba
simonjung <sjung@dendisoftware.com>
2023-02-20 21:27:10 -0500

    Added strobe

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import requests
+import time
 
 token = "c9f4bd850a7c8502dd5867967134bc916349025fd1a4a9ae5ced534df5376bb6"
 
@@ -6,4 +7,6 @@ headers = {
     "Authorization": "Bearer %s" % token,
 }
 
-response = requests.post('https://api.lifx.com/v1/lights/all/toggle', headers=headers)
+while True:
+    response = requests.post('https://api.lifx.com/v1/lights/all/toggle', headers=headers)
+    time.sleep(2)
